8dc1e6d6600fd2214ed712fe03517963e5b7f88a
Eduardo Gonçalves <edupersonalprojects@users.noreply.github.com>
2026-05-18 23:11:15 -0300

    :sparkles: [ui] identificacao da dor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Hero from "./_component/Hero";
+import IdentificationPainSection from "./_component/IdentificationPainSection";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <IdentificationPainSection />
     </main>
   );
 }
